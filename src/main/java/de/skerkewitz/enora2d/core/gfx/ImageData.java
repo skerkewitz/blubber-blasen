087280f9e9f3fc599e4d393e5df4fe2d81f99d3e
Stefan Kerkewitz <stefan.kerkewitz@gmail.com>
2019-02-24 17:26:40 +0100

    Remove unused code

--- src/main/java/de/skerkewitz/enora2d/core/gfx/ImageData.java
+++ src/main/java/de/skerkewitz/enora2d/core/gfx/ImageData.java
@@ -1,34 +1,15 @@
 package de.skerkewitz.enora2d.core.gfx;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
 public class ImageData {
 
   public final int width;
   public final int height;
   public int[] pixels;
 
-  public ImageData(String path) throws IOException {
-    BufferedImage image = ImageIO.read(ImageData.class.getResourceAsStream(path));
-
-    this.width = image.getWidth();
-    this.height = image.getHeight();
-
-    pixels = image.getRGB(0, 0, width, height, null, 0, width);
-
-
-    for (int i = 0; i < pixels.length; i++) {
-      /* We only care about four different values so we can flatten them. */
-      pixels[i] = (pixels[i] & 0xff) / 64;
-    }
-  }
 
   public ImageData(int width, int height, int[] pixels) {
     this.width = width;
     this.height = height;
     this.pixels = pixels;
   }
-
 }
