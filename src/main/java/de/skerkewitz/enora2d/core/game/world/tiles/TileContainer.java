b7bc97c904f469304f5bf4d0dc63a38aec42d8d3
Stefan Kerkewitz <stefan.kerkewitz@gmail.com>
2019-02-25 22:41:04 +0100

    Change color.

--- src/main/java/de/skerkewitz/enora2d/core/game/world/tiles/TileContainer.java
+++ src/main/java/de/skerkewitz/enora2d/core/game/world/tiles/TileContainer.java
@@ -9,7 +9,7 @@ public class TileContainer {
   public static final Tile STONE = new BasicSolidTile(1, 1, 0, RgbColorPalette.mergeColorCodes(-1, 5, -1, -1), 0xFF555555);
   public static final Tile GRASS = new BasicTile(2, 2, 0, RgbColorPalette.mergeColorCodes(-1, 131, 141, -1), 0xFF00FF00);
 
-  public static final Tile BB_STONE = new BasicSolidTile(4, 3, 0, RgbColorPalette.mergeColorCodes(-1, 502, 544, -1), 0xFF00FF00);
+  public static final Tile BB_STONE = new BasicSolidTile(4, 3, 0, RgbColorPalette.mergeColorCodes(-1, 502, 555, -1), 0xFF00FF00);
   public static final Tile WATER = new AnimatedTile(3, new int[][]{{0, 5}, {1, 5}, {2, 5}, {1, 5}},
           RgbColorPalette.mergeColorCodes(-1, 004, 115, -1), 0xFF0000FF, 1000);
 
